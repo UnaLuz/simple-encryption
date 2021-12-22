568413adab9a6091e5e394fc507cf9e71f6f56ff
UnaLuz <luz.cucarella97@gmail.com>
2021-12-21 21:28:30 -0300

    Use array adapter to set the drop-down menu options, also set the default value to simple substitution

--- app/src/main/java/com/unaluzdev/simpleencryption/MainActivity.kt
+++ app/src/main/java/com/unaluzdev/simpleencryption/MainActivity.kt
@@ -3,13 +3,29 @@ package com.unaluzdev.simpleencryption
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Get the strings for the menu and the dropdown view
+        val encryptionMethods = resources.getStringArray(R.array.encryptionMethods)
+        val dropdownMenu = findViewById<AutoCompleteTextView>(R.id.menuAutoCompleteTextView)
+        // Set the adapter
+        val adapter = ArrayAdapter(
+            this, R.layout.menu_list_item,
+            encryptionMethods
+        )
+        with(dropdownMenu){
+            setAdapter(adapter)
+            setText(getString(R.string.simple_substitution), false)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
